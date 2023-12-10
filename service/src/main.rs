9765a940c41f39d712526c4f2dee532d1e1e5192
David Pederson <dpederson@figure.com>
2023-12-10 10:20:36 -0700

    Add config

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,39 +1,26 @@
 use fauxstodian::{
     api::FauxstodianApi,
+    config::Config,
     driver::{SolanaDriver, SolanaRpc},
     proto::fauxstodian_server::FauxstodianServer,
     service::Service,
-};
-use solana_sdk::{
-    signature::Keypair,
-    signer::{EncodableKey, Signer},
 };
 use std::{error::Error, sync::Arc};
 use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // TODO: Read strings from env var config...
-    let listen_addr = "0.0.0.0:50055".parse().unwrap();
-    let solana_rpc_url = "http://127.0.0.1:8899";
-    let keypair_file = "dart.json";
+    let config = Config::default();
 
-    let dart_keypair = Keypair::read_from_file(keypair_file)?;
-    println!(
-        "Loaded dart public key: {:?}",
-        &dart_keypair.pubkey().to_string()
-    );
-
-    let rpc = SolanaRpc::new(solana_rpc_url, dart_keypair);
+    let rpc = SolanaRpc::new(config.rpc_url, config.keypair);
     let driver = Arc::new(Box::new(rpc) as Box<dyn SolanaDriver>);
-    let service = Service::new(driver); //Arc::clone(&driver));
-    let api = FauxstodianApi::new(service);
+    let api = FauxstodianApi::new(Service::new(driver));
 
-    println!("Fauxstodian server listening on {}", listen_addr);
+    println!("Fauxstodian server listening on {}", config.listen_addr);
 
     Server::builder()
         .add_service(FauxstodianServer::new(api))
-        .serve(listen_addr)
+        .serve(config.listen_addr)
         .await?;
 
     Ok(())
